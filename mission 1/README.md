0d7587f33faa542096270dc2cfbc8373e2f7fe18
Benny <benny.think@gmail.com>
2017-09-09 15:29:41 +0800

    Mission 1: parse csv file and import it to MySQL

---
+++ mission 1/README.md
@@ -0,0 +1,37 @@
+Intern-Life
+# My intern assignments in ***REMOVED***
+
+## Mission 1
+* Accomplish on 1st Sep 2017
+* Modify on 9th Sep 2017 (Only errors has been corrected)
+* Duration: about 1 day and two hours.
+
+
+## Description
+Parse the csv file and import it to MySQL
+
+
+## Steps
+No steps.
+
+
+## Errors
+* did not close file handler.
+
+
+## Suggestions by mentor(s)
+* use % to construct string
+* use `if __name__=='__main__'`
+* use `with` instead of `open`
+* use `mysql.connector` instead of `MySQLdb`
+* for list, prefer `append`
+* `os.path.join()`
+
+
+## Suggestions by myself
+* use `cur.execute('select * from table1 where id=%s',var)` to prevent from SQL Injection
+
+
+## Notes
+* use `%` and be prepared for SQL Injection.
+* clean code.
---
+++ mission 1/parse.py
@@ -0,0 +1,65 @@
+# coding:utf-8
+
+import MySQLdb
+import time
+import os
+import glob
+
+db = MySQLdb.connect("127.0.0.1", "root", "root", "test")
+cur = db.cursor()
+
+# each item of file_list is a file name, not absolute path!!
+file_list = [item for item in os.listdir(r'C:\Users\***REMOVED***\PycharmProjects\mission-1') if 'csv' in item]
+
+for i in range(len(file_list)):
+    file1 = open(file_list[i], 'r')
+    time_tuple = time.strptime(file1.name[22:40], '%b_%d_%Y_%H%M%S')
+    fn = file1.name[11:21]
+    dt = time.strftime('%Y-%m-%d %H:%M:%S', time_tuple)
+    # read all lines
+    temp = []
+    while True:
+        line = file1.readline()
+        temp.append(line)
+        if len(line) == 0:
+            break
+
+    for i in range(len(temp)):
+        if 'Node' in temp[i]:
+            node_name = temp[i][8:16]
+
+        if '776' in temp[i]:
+            # print temp[i - 1][31:34] + ' ' + temp[i].split()[1] + ' ' + temp[i].split()[3]
+            sql_str = "insert into table1 VALUES (NULL ," + \
+                      "'" + fn + "'," + \
+                      "'" + temp[i].split()[1] + "'," + \
+                      "'" + node_name + "'," + \
+                      "'" + temp[i - 1][31:34] + "'," + \
+                      "'" + temp[i].split()[3] + "','" + \
+                      dt + "')"
+            # execute sql command
+            cur.execute(sql_str)
+
+        elif '777' in temp[i]:
+            # print temp[i - 2][31:34] + ' ' + temp[i].split()[1] + ' ' + temp[i].split()[3]
+            sql_str = "insert into table1 VALUES (NULL ," + \
+                      "'" + fn + "'," + \
+                      "'" + temp[i].split()[1] + "'," + \
+                      "'" + node_name + "'," + \
+                      "'" + temp[i - 2][31:34] + "'," + \
+                      "'" + temp[i].split()[3] + "','" + \
+                      dt + "')"
+            cur.execute(sql_str)
+        elif '778' in temp[i]:
+            # print temp[i - 3][31:34] + ' ' + temp[i].split()[1] + ' ' + temp[i].split()[3]
+            sql_str = "insert into table1 VALUES (NULL ," + \
+                      "'" + fn + "'," + \
+                      "'" + temp[i].split()[1] + "'," + \
+                      "'" + node_name + "'," + \
+                      "'" + temp[i - 3][31:34] + "'," + \
+                      "'" + temp[i].split()[3] + "','" + \
+                      dt + "')"
+            cur.execute(sql_str)
+    file1.close()
+db.commit()
+db.close()
