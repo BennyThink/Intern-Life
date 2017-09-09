5dd6f734154bf2345377f8245a80ba8525e6f709
Benny <benny.think@gmail.com>
2017-09-09 15:30:09 +0800

    mission 2: MongoDB and MySQL, Python practive

---
+++ mission 2/README.md
@@ -0,0 +1,87 @@
+Intern-Life
+# My intern assignments in ***REMOVED***
+
+## Mission 
+* Accomplish on 9th Sep 2017
+* Modify on 
+* Duration: one day and a half
+
+
+## Description
+* set up MySQL and MongoDB in a minimal CentOS with password authentication.
+* import sql file to MySQL
+* write a Python to import data from MySQL to MongoDB
+* design a Python function: query from MongoDB according to an IP address
+* design a Python function: query from MongoDB according to a regex expression of device name
+
+
+## Steps
+### epel release
+`yum install -y epel-release`
+### install MySQL(Build from source)
+Due to network issues, decided to build from source. `lnmp.org` one key script.
+### install MongoDB(from [official repository](https://docs.mongodb.com/master/tutorial/install-mongodb-on-red-hat/))
+```
+vim /etc/yum.repos.d/mongodb-org-3.4.repo
+
+#write the following content
+[mongodb-org-3.4]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.4/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-3.4.asc
+
+#install MongoDB
+yum install -y mongodb-org
+
+#disable SELinux, choose one
+semanage port -a -t mongod_port_t -p tcp 27017
+#or
+vim /etc/selinux/config
+SELINUX=disabled
+#or
+SELINUX=permissive
+
+#reboot then, or just simpley setenforce 
+service mongod start
+```
+### MongoDB authentication
+Create a root user first
+```
+use admin
+db.createUser(
+{
+user: "root",
+pwd: "***REMOVED***123",
+roles: [ { role: "root", db: "admin" } ]
+}
+)
+```
+Enable authentication
+```
+vim /etc/mongod.conf
+
+#add following content, for windows use --auth instead
+security:
+  authorization: enabled
+  
+#restart MongoDB
+service mongod restart
+```
+###import data to MySQL
+`mysql -u root -p db1 < test.sql`
+
+
+## Errors
+
+
+## Suggestions by mentor(s)
+
+
+## Suggestions by myself
+
+
+## Afterwards
+
+## Notes
---
+++ mission 2/mysql2mongodb.py
@@ -0,0 +1,101 @@
+#!/usr/bin/python
+#  coding:utf-8
+
+# import data from MySQL to MongoDB,
+# clear MongoDB,
+# search according to IP and device name(regex)
+
+import mysql.connector
+from pymongo import MongoClient, errors
+import pprint
+
+mongo_dic = {}
+mongo_client = MongoClient('127.0.0.1', username='root', password='***REMOVED***123',
+                           authMechanism='SCRAM-SHA-1')
+db = mongo_client['san']
+col = db['san_device']
+
+
+def read_mysql():
+    # read from MySQL and  insert into MongoDB one by one
+    con = mysql.connector.connect(user='root', password='root',
+                                  host='127.0.0.1',
+                                  database='san')
+
+    cur = con.cursor()
+    cur.execute('select * from san_device')
+    data = cur.fetchall()
+
+    for i in range(len(data)):
+        mongo_dic.update(id=data[i][0], devicename=data[i][1], domainid=data[i][2],
+                         ip=data[i][3], cp0ip=data[i][4], cp1ip=data[i][5],
+                         location=data[i][6], hasfillword=data[i][7],
+                         username=data[i][8], password=data[i][9],
+                         firmware=data[i][10], vfname=data[i][11], slotinfo=data[i][12],
+                         switchrole=data[i][13], zoning=data[i][14], wwn=data[i][15],
+                         area=data[i][16], factory=data[i][17])
+
+        write_mongo(mongo_dic)
+    con.close()
+    mongo_client.close()
+
+
+def write_mongo(data_dic):
+    try:
+        # use copy() to prevent from Duplicate Key Error
+        col.insert(data_dic.copy())
+    except errors.PyMongoError as e:
+        print 'Operation failed...', e
+    else:
+        print 'Operation succeed'
+
+
+def remove_all_document():
+    try:
+        col.remove()
+    except errors.PyMongoError as e:
+        print 'Operation failed...', e
+    else:
+        print 'Operation succeed'
+    finally:
+        mongo_client.close()
+
+
+def ip_query(ip):
+    ip_query_result = col.find_one({'ip': ip})
+    if ip_query_result is None:
+        print 'No such IP...'
+    else:
+        pprint.pprint(ip_query_result)
+
+
+def dn_query(dn):
+    dn_query_result = col.find({'devicename': {'$regex': dn}})
+    if dn_query_result is None:
+        print 'No such device...'
+    else:
+        for post in dn_query_result:
+            pprint.pprint(post)
+
+
+if __name__ == '__main__':
+
+    print '---------------------------------------------'
+    print 'select your actions ^_^'
+    print '1. import data from MySQL to Mongodb'
+    print '2. remove all documents in Mongodb'
+    print '3. query by IP in Mongodb'
+    print '4. query by device name(regex) in Mongodb'
+    print '---------------------------------------------'
+
+    choice = input('>')
+    if choice == 1:
+        read_mysql()
+    elif choice == 2:
+        remove_all_document()
+    elif choice == 3:
+        ip_query(raw_input('Input your IP>>'))
+    elif choice == 4:
+        dn_query(raw_input('Input your regex expression>>'))
+    else:
+        print 'Uhh...Wrong choice. Exit now.'
