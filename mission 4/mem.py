1e80ec6b97bdbe91dd706f77eb89467a3eec8caf
Benny <benny.think@gmail.com>
2017-11-17 15:06:16 +0800

    fix

--- mission 4/mem.py
+++ mission 4/mem.py
@@ -4,6 +4,8 @@
 # Intern-Life - mem.py
 # 2017/11/16 9:25
 # Python 2 ONLY.
+# 2017-11-17 15:03:01
+# This time it seems right...
 
 __author__ = 'Benny <benny@bennythink.com>'
 
@@ -11,38 +13,40 @@ import sys
 import mysql.connector
 import time
 
-SIZE = 0
-
 
 def recursive(var):
+    """
+    calculate variables' memory consumption
+    :param var: str,float,unicode,int,list and tuple only.
+    :return: size in bytes.
+    """
 
-    global SIZE
-    if isinstance(var, str) or isinstance(var, unicode) or isinstance(var, int) or isinstance(var, float):
-        SIZE += sys.getsizeof(var)
-    elif isinstance(var, list) or isinstance(var, tuple):
-        for i in var:
-            if isinstance(i, list) or isinstance(i, tuple):
-                recursive(i)
-            elif isinstance(i, str) or isinstance(i, unicode) or isinstance(i, int) or isinstance(i, float):
-                # Strange...?
-                SIZE += sys.getsizeof(var)
-                break
-    elif isinstance(var, dict):
-        pass
+    if isinstance(var, (int, float, unicode, str)):
+        return sys.getsizeof(var)
+
     else:
-        SIZE += sys.getsizeof(var)
-        recursive(var)
+        size = sys.getsizeof(var)
+        for i in var:
+            size += recursive(i)
+        return size
 
 
 def test():
-    con = mysql.connector.connect(user='root', password='root', database='san')
+    """
+    get big result set.
+    :return: result set in the form of [(...), (...)]
+    """
+    con = mysql.connector.connect(user='root', password='root', database='info')
     cur = con.cursor()
     cur.execute('select * from san_device')
-    data = cur.fetchmany(2)
-    recursive(data)
-    print SIZE / 1024.0 / 1024.0
-    time.sleep(100)
+    data = cur.fetchmany(50000)
+    return data
 
 
 if __name__ == '__main__':
-    test()
+    s = [(1, 2, 3), (1, 2)]
+    # Notes:
+    # 50000     100000      200000
+    # 49.9-41   95.1-84     185.9-169
+    print recursive(test()) / 1024.0 / 1024.0
+    time.sleep(100)
