b81767ee7dcbaa042cd1dfc3c0a341ac1d35c130
Benny <benny.think@gmail.com>
2018-01-05 14:03:34 +0800

    omg

--- mission 6/fast.py
+++ mission 6/fast.py
@@ -18,10 +18,57 @@ con = mysql.connector.connect(host='127.0.0.1', user='root', password='root', da
 cur = con.cursor()
 
 
+class Create(tornado.web.RequestHandler):
+
+    def get(self):
+        self.render('index.html')
+
+    def post(self):
+        cmd = "INSERT INTO test VALUES (%s,%s,%s,%s,%s)"
+        col1 = self.get_argument('col1')
+        col2 = self.get_argument('col2')
+        col3 = self.get_argument('col3')
+        col4 = self.get_argument('col4')
+        col5 = self.get_argument('col5')
+        cur.execute(cmd, (col1, col2, col3, col4, col5))
+        con.commit()
+        self.redirect('/')
+
+
 class Retreive(tornado.web.RequestHandler):
 
     def get(self):
         self.write(get_data())
+
+
+class Update(tornado.web.RequestHandler):
+
+    def get(self):
+        self.render('index.html')
+
+    def post(self):
+        cmd = "UPDATE test SET name=%s,ip=%s,platform=%s,hardware=%s WHERE id=%s"
+        col1 = self.get_argument('col1')
+        col2 = self.get_argument('col2')
+        col3 = self.get_argument('col3')
+        col4 = self.get_argument('col4')
+        col5 = self.get_argument('col5')
+        cur.execute(cmd, (col2, col3, col4, col5, col1))
+        con.commit()
+        self.redirect('/')
+
+
+class Delete(tornado.web.RequestHandler):
+
+    def get(self):
+        self.render('index.html')
+
+    def post(self):
+        cmd = "DELETE FROM test WHERE id=%s"
+        col1 = self.get_argument('col1')
+        cur.execute(cmd, (col1,))
+        con.commit()
+        self.redirect('/')
 
 
 class Index(tornado.web.RequestHandler):
@@ -30,22 +77,34 @@ class Index(tornado.web.RequestHandler):
         self.render('index.html')
 
 
-settings = {'template_path': 'templates', 'debug': 'true',
-            'static_path': os.path.join(os.path.dirname(__file__), "static")}
-
-
 def make_app():
     return tornado.web.Application([
+        (r'/add/', Create),
         (r'/list/', Retreive),
+        (r'/update/', Update),
+        (r'/delete/', Delete),
         (r'/', Index),
     ],
-        **settings)
+        template_path=os.path.join(os.path.dirname(__file__), "templates"),
+        static_path=os.path.join(os.path.dirname(__file__), "static"),
+        debug=True
+    )
 
 
 def get_data():
-    with open('test.json', 'r') as f:
-        data = f.read()
-    return json.loads(data)
+    cur.execute('SHOW COLUMNS from test')
+    col_data = cur.fetchall()
+    col_field = [i[0] for i in col_data]
+
+    cur.execute('SELECT * FROM test')
+    data = cur.fetchall()
+    bulk_dic = []
+    for i in range(len(data)):
+        es_dic = dict(zip(col_field, data[i]))
+        bulk_dic.append(es_dic)
+
+    target = {"data": bulk_dic}
+    return json.dumps(target)
 
 
 if __name__ == '__main__':
